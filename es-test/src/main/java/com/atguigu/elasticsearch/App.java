0d45603b067afb388fefc9bca44a8f919dd5cccd
huahua <1942207534@qq.com>
2024-01-10 18:11:37 +0800

    fot-fix分支提交7

--- es-test/src/main/java/com/atguigu/elasticsearch/App.java
+++ es-test/src/main/java/com/atguigu/elasticsearch/App.java
@@ -13,12 +13,6 @@ public class App
         System.out.println( "Hello World!hot-fix" );
         System.out.println( "Hello World!hot-fix" );
         System.out.println( "Hello World!hot-fix" );
-        System.out.println( "Hello World!hot-fix111" );
- System.out.println( "Hello World!" ); System.out.println( "Hello World!" ); System.out.println( "Hello World!" ); System.out.println( "Hello World!" ); System.out.println( "Hello World!" ); System.out.println( "Hello World!" ); System.out.println( "Hello World!" );
-        System.out.println( "Hello World!hot-fix222" );        System.out.println( "Hello World!hot-fix222" );        System.out.println( "Hello World!hot-fix222" );
-        System.out.println( "Hello World!hot-fix222" );        System.out.println( "Hello World!hot-fix222" );        System.out.println( "Hello World!hot-fix222" );
-
-        System.out.println( "Hello World!hot-fix222" );        System.out.println( "Hello World!hot-fix222" );        System.out.println( "Hello World!hot-fix222" );
 
     }
 }
